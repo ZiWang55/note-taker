47e695257126ac21e08ee0071d45cd871ca189e1
jpd61 <jpd@dewoodyoil.com>
2020-05-25 11:38:11 -0500

    setup express and routes

---
+++ server.js
@@ -0,0 +1,20 @@
+const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3003;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
