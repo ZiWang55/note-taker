09980b96a9472e7d12b8f4fa863f80af78ac0444
jpd61 <jpd@dewoodyoil.com>
2020-05-25 13:30:08 -0500

    refactor

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 3003;
+const htmlRoutes = require("./routes/htmlRoutes");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+require("./routes/apiRoutes")(app);
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
-
-require("./routes/apiRoutes")(app);
-const htmlRoutes = require("./routes/htmlRoutes");
-
 app.use('/', htmlRoutes);
 
 app.listen(PORT, function() {
